283ea6da5c8b621480f9d30e55a7863bcff40cce
vladislavv82 <vladislav.akopian82@gmail.com>
2023-03-01 12:55:03 +0300

    calculator controller

--- src/main/java/ru/akopian/spring/controllers/FirstController.java
+++ src/main/java/ru/akopian/spring/controllers/FirstController.java
@@ -1,16 +1,26 @@
 package ru.akopian.spring.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+//TODO create global git tocen
 @Controller
 //@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
-return "first/hello";
+    public String helloPage(@RequestParam("name") String name,
+                            @RequestParam("surname") String surname,
+                            Model model){
+
+        //System.out.println("Hello "+ name + " " + surname);
+        model.addAttribute("message","Hello "+ name + " " + surname);
+        return "first/hello";
     }
     @GetMapping("/goodbye")
     public String goodByePage(){
