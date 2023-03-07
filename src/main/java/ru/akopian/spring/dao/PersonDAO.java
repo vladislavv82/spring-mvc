8de1f509858431ba74b410a547565115be4e0ca9
vladislavv82 <vladislav.akopian82@gmail.com>
2023-03-07 17:36:48 +0300

    upd: postgresql

--- src/main/java/ru/akopian/spring/dao/PersonDAO.java
+++ src/main/java/ru/akopian/spring/dao/PersonDAO.java
@@ -3,6 +3,7 @@ package ru.akopian.spring.dao;
 import org.springframework.stereotype.Component;
 import ru.akopian.spring.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,32 +11,85 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
 
-    {
-        people = new ArrayList<>();
+    private static final String URL = "jdbc:postgresql://localhost:5432/spring-app";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
-        people.add(new Person(++PEOPLE_COUNT,"Vlad" ));
-        people.add(new Person(++PEOPLE_COUNT,"Artem" ));
-        people.add(new Person(++PEOPLE_COUNT,"Tom" ));
+    private static final Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("BD Error");
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-    public List<Person> index(){
+
+    public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement(); // содержит в себе запрос к базе дынных
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL); // строки которые вернулись с дб
+            //executeQuery выполняет запрос и возвращает данные
+
+            //переводим в Джава объект строки которые вернулись
+            //итератор
+            while (resultSet.next()) {
+                Person person = new Person();
+
+                //для каждой строки получаем значение и записываем её в объект
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                //внесенные изменения в объекте person запиываем в список
+                people.add(person);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return people;
     }
-    public Person show(int id){
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-    }
-    public void save(Person person){
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
-    }
-    public void update( int id, Person updatePerson){
-        Person personToBeUpdated = show(id);
 
-        personToBeUpdated.setName(updatePerson.getName());
-    }
-    public void delete(int id){
-        people.removeIf(p -> p.getId() == id );
-    }
 
+//    public Person show(int id) {
+//        //     return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+//    }
+
+    public void save(Person person) {
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() +
+                    "'," + person.getAge() + ",'" + person.getEmail() + "')";
+
+            statement.executeUpdate(SQL);
+            //executeUpdate обновляет и изменяет данные в базе
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+//    public void update(int id, Person updatedPerson) {
+//        Person personToBeUpdated = show(id);
+//
+//        personToBeUpdated.setName(updatedPerson.getName());
+//        personToBeUpdated.setAge(updatedPerson.getAge());
+//        personToBeUpdated.setEmail(updatedPerson.getEmail());
+//    }
+//
+//    public void delete(int id) {
+//        people.removeIf(p -> p.getId() == id);
+//    }
+//}
--- src/main/java/ru/akopian/spring/models/Person.java
+++ src/main/java/ru/akopian/spring/models/Person.java
@@ -1,15 +1,30 @@
 package ru.akopian.spring.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class Person {
     private int id;
+    @NotEmpty(message = "Name shoud not be emty")
+    @Size(min = 2,max = 20, message = "Name be between 2 and 20")
     private String name;
+    @Min(value = 0, message = "Age > 0")
+    private int age;
+    @NotEmpty(message = "Email invalid")
+    @Email
+    private String email;
 
-    public Person(){
+    public Person() {
 
     }
-    public Person(int id, String name) {
+
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -26,5 +41,21 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
